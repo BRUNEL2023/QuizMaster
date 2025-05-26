b5ca109c939ef81f4961a800656f97bbb38759fe
BRUNEL2023 <153836375+BRUNEL2023@users.noreply.github.com>
2025-05-26 12:36:51 +0200

    2ème Commit

--- app/src/main/java/com/example/quizmaster/ui/theme/QuizDayActivity.kt
+++ app/src/main/java/com/example/quizmaster/ui/theme/QuizDayActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizmaster.ui.theme
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quizmaster.R
@@ -11,6 +12,10 @@ class QuizDayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_day)
+
+
+        val imgProfile = findViewById<ImageView>(R.id.imgProfile)
+        val imgSettings = findViewById<ImageView>(R.id.imgSettings)
 
         val chapitre = intent.getStringExtra("chapitre") ?: "Chapitre inconnu"
         val matiere = intent.getStringExtra("matiere") ?: "Matière inconnue"
@@ -21,6 +26,15 @@ class QuizDayActivity : AppCompatActivity() {
         val recap = findViewById<TextView>(R.id.recap)
         recap.text = "Prépare-toi à tester tes connaissances sur \"$chapitre\"."
 
+        imgSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        imgProfile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
         val startBtn = findViewById<Button>(R.id.startQuizBtn)
         startBtn.setOnClickListener {
             val intent = Intent(this, QuestionActivity::class.java)
